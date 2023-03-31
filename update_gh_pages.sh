e1ebebffe606d6e20db60ad31545a5bfc86a00a9
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2023-03-31 20:20:07 +1000

    Add script to update WASM

---
+++ update_gh_pages.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Builds and updates the local `gh-pages` branch from the latest source.
+#
+# Doesn't require checking out the `gh-pages` branch.
+
+set -e
+set -u
+
+cargo build --release --target=wasm32-unknown-unknown
+wasm-bindgen --target web --out-dir static/ target/wasm32-unknown-unknown/release/rtttlsynth.wasm
+
+target_branch="gh-pages"
+current_commit_hash=$(git rev-parse $target_branch)
+
+export GIT_INDEX_FILE=$(mktemp)
+git read-tree --empty
+
+files=(index.html rtttlsynth.js rtttlsynth_bg.wasm)
+for file in "${files[@]}"; do
+  git update-index --add --cacheinfo 100644 "$(git hash-object -w "static/$file")" "$file"
+done
+
+tree_hash=$(git write-tree)
+
+rm "$GIT_INDEX_FILE"
+unset GIT_INDEX_FILE
+
+new_commit_hash=$(git commit-tree $tree_hash -p $current_commit_hash -m "Build from latest source")
+git update-ref "refs/heads/$target_branch" $new_commit_hash
